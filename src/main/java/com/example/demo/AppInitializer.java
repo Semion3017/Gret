8cb0a8c0bb90868a18e9aba743e964573ef87337
Semion3017 <Semen3017@yandex.by>
2023-08-30 12:09:57 +0300

    f

--- src/main/java/com/example/demo/AppInitializer.java
+++ src/main/java/com/example/demo/AppInitializer.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import org.springframework.web.WebApplicationInitializer;
+
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRegistration;
+
 
 public class AppInitializer implements WebApplicationInitializer {
 
@@ -27,6 +30,10 @@ public class AppInitializer implements WebApplicationInitializer {
         servlet.setLoadOnStartup(1);
 		
 	}
+
+
+
+	
     
 }
 
